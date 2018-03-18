82a2a04cb5eef9ec668b9c28790d1acbb056f391
Dmitriy Ostrovskiy <dmtrostrovskiy@gmail.com>
2018-03-17 22:06:53 -0700

    Update the Fighter Class

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,18 @@
 // contstruct a player class
-
+class Fighter {
+    constructor(hp, attack, counter) {
+        this.hp = hp;
+        this.attack = attack;
+        this.counter = counter;
+        this.attackTimes = 1;
+        this.updatedAttack;
+        this.isAttacking = false;
+    }
+    attack(){
+        this.attackTimes++;
+        this.updatedAttack = this.attack * this.attackTimes;
+    }
+};
 
 
 // have an array of all characters and their specs
